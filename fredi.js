6d64ab152d23da728ebfa25e0e17da65c8c3cf62
FeeSpace <frediezra001@gmail.com>
2025-12-19 20:52:17 +0300

    Update fredi.js

--- fredi.js
+++ fredi.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8000;
 let server = require('./qr'),
     code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/frediqr', server);
+app.use('/qr', server);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/fredipair.html')
